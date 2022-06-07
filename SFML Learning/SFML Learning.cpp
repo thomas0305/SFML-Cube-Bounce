1a94b8ff3d9ea5f12a7bdd2437aa6cf9967601d5
Thomas <Thomas@DESKTOP-MTA6TLQ>
2022-06-07 17:23:49 +0200

    updated stuff

--- SFML Learning/SFML Learning.cpp
+++ SFML Learning/SFML Learning.cpp
@@ -1,19 +1,31 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
 
 // https://www.youtube.com/watch?v=R87RUYUfgwM
 
+int cornerHit() {
+    static int cornerCount = 0;
+    cornerCount++;
+    return cornerCount;
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "My Program");
+    std::cout << "This is a test \n";
+
+
+    constexpr int windowWidth = 1280, windowHeight = 720;
+
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight, "My Program");
     window.setFramerateLimit(60);
 
     sf::RectangleShape rect;
 
-    sf::Vector2f rectanglePosition(300, 200);
+    sf::Vector2f rectanglePosition(0, 0);
 
 
-    int rectHeight = 100, rectWidth = 100;
-
+    constexpr int rectHeight = 100, rectWidth = 100;
 
     rect.setPosition(rectanglePosition);
     rect.setSize(sf::Vector2f(rectHeight, rectWidth));
@@ -21,15 +33,32 @@ int main()
     float xVelocity{ 3 };
     float yVelocity{ 3 };
 
+
+
     while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {
+          while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) 
                 window.close();
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) 
                 window.close();
         }
+
+        //Checks if cube hits corner, will print to console if it does.
+        if (rectanglePosition.x == 0 && rectanglePosition.y == 0) {
+            std::cout << "The cube has hit the corner " << cornerHit() << " times. \n";
+        }
+        if (rectanglePosition.x == 1280 - rectHeight && rectanglePosition.y == 0) {
+            std::cout << "The cube has hit the corner " << cornerHit() << " times. \n";
+        }
+        if (rectanglePosition.x == 0 && rectanglePosition.y == 720 - rectWidth) {
+            std::cout << "The cube has hit the corner " << cornerHit() << " times. \n";
+        }
+        if (rectanglePosition.x == 1280 - rectHeight && rectanglePosition.y == 720 - rectWidth) {
+            std::cout << "The cube has hit the corner " << cornerHit() << " times. \n";
+        }
+
 
         //physics
         if (rectanglePosition.x < 0 || rectanglePosition.x > 1280 - rectHeight) {
